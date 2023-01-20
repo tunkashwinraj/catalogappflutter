c97f625588ff903fa9ae9fdb23961fa20ea41a8d
tunkashwinraj <tunkashwin96@gmail.com>
2023-01-20 06:12:14 -0800

    day4 completed

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,10 @@ import 'dart:js_util';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:practise_mod/pages/login_page.dart';
 import 'pages/home_page.dart';
+
 void main(List<String> args) {
   runApp(MyApp());
 }
@@ -16,18 +18,18 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       themeMode: ThemeMode.light,
       theme: ThemeData(
-        primarySwatch: Colors.deepOrange
-      ),
-      darkTheme: ThemeData(
-        brightness: Brightness.light 
-      ),
-      initialRoute: "/home",
+          primarySwatch: Colors.deepOrange,
+          fontFamily: GoogleFonts.lato().fontFamily,
+          primaryTextTheme: GoogleFonts.latoTextTheme()),
+      darkTheme: ThemeData(brightness: Brightness.light),
+      initialRoute: "/login",
       routes: {
-        "/":(context) => LoginPage(),
-        "/home":(context) => HomePage(),
-        "/login":(context) => LoginPage()
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage()
       },
-      
     );
   }
+
+  GoogleFonts newMethod() => GoogleFonts();
 }
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
@@ -6,16 +8,52 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Container(
-        child: Center(
-          child: Text("Login Page",
-          style: TextStyle(
-            fontSize: 40,
-            color: Colors.redAccent,
-            fontWeight: FontWeight.w100
+      color: Colors.white,
+      child: Column(
+        children: [
+          Image.asset(
+            "assets/images/login_image.png",
+            fit: BoxFit.cover,
           ),
+          SizedBox(
+            height: 20,
           ),
-        ),
+          Text(
+            "Welcome",
+            style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 32),
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                      hintText: "Enter username", labelText: "Username"),
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      hintText: "Enter password", labelText: "password"),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      print("Hello Ashwin raj");
+                    },
+                    child: Text("Login"),
+                    style: TextButton.styleFrom()),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
